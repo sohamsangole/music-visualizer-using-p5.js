8a98c2290547e42a71d738771ae59e03cb800c99
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-07 13:26:45 +0530

    Added Navbar

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="fixed top-0 left-0 w-full p-4 z-10">
+            <div className="max-w-7xl mx-auto flex justify-between items-center text-white">
+                <div className="text-2xl font-bold">meowzic</div>
+                <div className="space-x-4">
+                    <a href="#home" className="font-thin hover:text-gray-300">Home</a>
+                    <a href="#visualizer" className="font-thin hover:text-gray-300">Visualizer</a>
+                    <a href="#about" className="font-thin hover:text-gray-300">About</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 "use client";
 
 import HomePage from "./components/HomePage";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <HomePage />
+      <NavBar />
     </div>
   );
 }
