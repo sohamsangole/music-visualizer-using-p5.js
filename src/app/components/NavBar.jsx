c26b9c21d115327baa278686cf5650f0b4ab4dc6
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-07 14:40:21 +0530

    Updated Navbar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'; // Using next/link for client-side navigation
 
 const NavBar = () => {
     return (
@@ -6,9 +7,9 @@ const NavBar = () => {
             <div className="max-w-7xl mx-auto flex justify-between items-center text-white">
                 <div className="text-2xl font-bold">meowzic</div>
                 <div className="space-x-4">
-                    <a href="#home" className="font-thin hover:text-gray-300">Home</a>
-                    <a href="#visualizer" className="font-thin hover:text-gray-300">Visualizer</a>
-                    <a href="#about" className="font-thin hover:text-gray-300">About</a>
+                    <Link href="/" className="font-thin hover:text-gray-300">Home</Link>
+                    <Link href="/visualizer" className="font-thin hover:text-gray-300">Visualizer</Link>
+                    <Link href="/about" className="font-thin hover:text-gray-300">About</Link>
                 </div>
             </div>
         </nav>
